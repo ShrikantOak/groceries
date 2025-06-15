3e3b6ef6441b35d18df8e04c33e3af8d931a7a26
Shrikant Oak <oak@feamold.com>
2025-06-15 16:12:30 -0400

    Added typo

--- app/groceries/page.tsx
+++ app/groceries/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/lib/supabase/server';
 
 export default async function Groceries() {
   const supabase = await createClient();
-  const { data: groceries } = await supabase.from("groceries").select();
+  const { data: groceries } = await supabase.from("groceu6ries").select();
 
   return <pre>{JSON.stringify(groceries, null, 2)}</pre>
 }
